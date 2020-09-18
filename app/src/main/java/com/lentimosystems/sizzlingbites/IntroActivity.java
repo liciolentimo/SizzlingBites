6971902155be5ff0d2a3baed53b19e2ece8708ad
liciolentimo <lentimo@gmail.com>
2020-09-18 10:04:50 +0300

    added walkthrough screens

--- app/src/main/java/com/lentimosystems/sizzlingbites/IntroActivity.java
+++ app/src/main/java/com/lentimosystems/sizzlingbites/IntroActivity.java
@@ -1,17 +1,29 @@
 package com.lentimosystems.sizzlingbites;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.lentimosystems.sizzlingbites.Fragments.OnboardingFragmentOne;
+import com.lentimosystems.sizzlingbites.Fragments.OnboardingFragmentThree;
+import com.lentimosystems.sizzlingbites.Fragments.OnboardingFragmentTwo;
 
 public class IntroActivity extends AppCompatActivity {
     ImageView logo, img;
     LottieAnimationView mLottieAnimationView;
     TextView title;
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager mViewPager;
+    private ScreenSlidePagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +35,42 @@ public class IntroActivity extends AppCompatActivity {
         mLottieAnimationView = findViewById(R.id.lottie);
         title = findViewById(R.id.title);
 
+        mViewPager = findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+
         img.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
         logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
         mLottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
         title.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnboardingFragmentOne one = new OnboardingFragmentOne();
+                    return one;
+                case 1:
+                    OnboardingFragmentTwo two = new OnboardingFragmentTwo();
+                    return two;
+                case 2:
+                    OnboardingFragmentThree three = new OnboardingFragmentThree();
+                    return three;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
